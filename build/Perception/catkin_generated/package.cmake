365fa8fb6b4fc86fb5c74b0c7d5d385c77a3a4f7
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-05-30 01:04:10 -0400

    build perception

--- build/Perception/catkin_generated/package.cmake
+++ build/Perception/catkin_generated/package.cmake
@@ -1,5 +1,5 @@
 set(_CATKIN_CURRENT_PACKAGE "Perception")
-set(Perception_MAINTAINER "parthmehrotra <parthmehrotra@todo.todo>")
+set(Perception_MAINTAINER "Parth Mehrotra <parth.mehrotra@driveai.org>")
 set(Perception_DEPRECATED "")
 set(Perception_VERSION "0.0.0")
 set(Perception_BUILD_DEPENDS "roscpp" "rospy" "std_msgs")
